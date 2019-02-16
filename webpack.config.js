1dbed5580a5a3c5e62512fe886e68ab4ea9fabb0
Mikhail Istomin <redeemer@inbox.ru>
2019-02-16 23:06:23 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  mode: "development"
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: "all"
+  //   }
+  // }
+};
