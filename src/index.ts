d34c5d7a0cbe242a2fa814154493d981bde18591
Mikhail Istomin <redeemer@inbox.ru>
2019-02-18 23:01:20 +0300

    shape type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Block, BlockIO } from "../typing/block";
+import { Shape, ShapeForm } from "../typing/shape";
 import { Board } from "../typing/board";
 
 var board: Board = { width: 10, height: 20, blocks: [] };
-var testBlock: Block = { x: 3, y: 5 };
-board.blocks.push(testBlock);
 
 // create board
 var target = document.getElementById("target");
@@ -13,30 +12,37 @@ for (let rowIndex = 0; rowIndex < board.height; rowIndex++) {
     cellEl.classList.add("cell");
     cellEl.dataset.x = rowIndex.toString();
     cellEl.dataset.y = colIndex.toString();
-    target.appendChild(cellEl)
+    target.appendChild(cellEl);
   }
 }
 
-var animate = function () {
+// add test shape
+var shape: Shape = {
+  form: ShapeForm.Square,
+  blocks: [{ x: 6, y: 6 }, { x: 7, y: 6 }, { x: 6, y: 7 }, { x: 7, y: 7 }]
+};
+shape.blocks.forEach(b => board.blocks.push(b));
+
+var animate = function() {
   requestAnimationFrame(animate);
-  Array
-    .from(document.querySelectorAll(".cell"))
+  Array.from(document.querySelectorAll(".cell"))
     .map(el => ({
       el,
       canvasBlock: getCanvasBlockByEl(board, el as HTMLElement)
     }))
     .forEach(x => {
       if (x.canvasBlock) {
-        x.el.classList.add("block")
+        x.el.classList.add("block");
       } else {
-        x.el.classList.remove("block")
+        x.el.classList.remove("block");
       }
-
-    })
+    });
 };
 
 animate();
 
 function getCanvasBlockByEl(board: Board, el: HTMLElement): Block {
-  return board.blocks.find(b => b.x.toString() === el.dataset.x && b.y.toString() === el.dataset.y)
+  return board.blocks.find(
+    b => b.x.toString() === el.dataset.x && b.y.toString() === el.dataset.y
+  );
 }
--- typing/shape.ts
+++ typing/shape.ts
@@ -1,8 +1,29 @@
 import * as t from "io-ts";
 import { BlockIO } from "./block";
 
+export enum ShapeForm {
+  Square = "Square",
+  Cripple = "Cripple",
+  CrippleRev = "CrippleRev",
+  Pipe = "Pipe",
+  PipeRev = "PipeRev",
+  Sausage = "Sausage",
+  Cross = "Cross"
+}
+
+const ShapeFormV = t.keyof({
+  [ShapeForm.Square]: null,
+  [ShapeForm.Cripple]: null,
+  [ShapeForm.CrippleRev]: null,
+  [ShapeForm.Pipe]: null,
+  [ShapeForm.PipeRev]: null,
+  [ShapeForm.Sausage]: null,
+  [ShapeForm.Cross]: null
+});
+
 export const ShapeIO = t.type({
-  blocks: t.array(BlockIO)
+  blocks: t.array(BlockIO),
+  form: ShapeFormV
 });
 
 export type Shape = t.TypeOf<typeof ShapeIO>;
