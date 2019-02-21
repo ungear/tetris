1211c877a4b20da7a64b89c816addbd01c81022e
Mikhail Istomin <redeemer@inbox.ru>
2019-02-21 23:49:35 +0300

    basic tests

---
+++ spec/logic.spec.ts
@@ -0,0 +1,35 @@
+import { Block, BlockIO } from "../typing/block";
+import { Shape, ShapeForm } from "../typing/shape";
+import { Board } from "../typing/board";
+import { Game } from "../typing/game";
+
+import {
+  getBoardFragmentByCoords,
+  getNewShape,
+  isShapeLandedOnFragment,
+  isShapeLandedOnBottom,
+  moveShapeDown
+} from "../src/logic";
+
+describe("isShapeLandedOnBottom", () => {
+  it("should return false for shape not in the bottom", () => {
+    let game: Game = {
+      shape: {
+        form: ShapeForm.Square,
+        blocks: [{ x: 1, y: 1 }]
+      },
+      board: { width: 10, height: 10, fragments: [] }
+    };
+    expect(isShapeLandedOnBottom(game)).toBeFalsy();
+  });
+  it("should return true for shape in the bottom", () => {
+    let game: Game = {
+      shape: {
+        form: ShapeForm.Square,
+        blocks: [{ x: 1, y: 9 }]
+      },
+      board: { width: 10, height: 10, fragments: [] }
+    };
+    expect(isShapeLandedOnBottom(game)).toBeTruthy();
+  });
+});
--- spec/test.spec.ts
+++
@@ -1,8 +0,0 @@
-import "jasmine";
-//import something from "../src/something";
-
-describe("something", () => {
-  it("should work", () => {
-    expect(1).toBe(1);
-  });
-});
