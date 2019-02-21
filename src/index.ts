8f330745fd06d4e52b6fdba434ece8cfae642179
Mikhail Istomin <mikhail_istomin@epam.com>
2019-02-21 09:26:57 +0300

    falling workflow

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Block, BlockIO } from "../typing/block";
 import { Shape, ShapeForm } from "../typing/shape";
 import { Board } from "../typing/board";
-import { Game } from "../typing/game"
-import { interval } from 'rxjs';
+import { Game } from "../typing/game";
+import { interval } from "rxjs";
 
 const FALLING_INTERVAL_MS = 1000;
 
-var board: Board = { width: 10, height: 20, blocks: [] };
+var board: Board = { width: 10, height: 20, fragments: [] };
 
 // create board
 var target = document.getElementById("target");
@@ -25,29 +25,57 @@ var shape: Shape = {
   form: ShapeForm.Square,
   blocks: [{ x: 1, y: 6 }, { x: 2, y: 6 }, { x: 1, y: 7 }, { x: 2, y: 7 }]
 };
-shape.blocks.forEach(b => board.blocks.push(b));
 
 var game: Game = {
   shape,
   board
-}
+};
 
 const falling$ = interval(FALLING_INTERVAL_MS);
 falling$.subscribe(_ => {
-  moveShapeDown(game.shape)
-
-})
+  if (isShapeLandedOnFragment(game) || isShapeLandedOnBottom(game)) {
+    //add shape to fragments
+    //calculate row to destroy
+    //new shape
+  } else {
+    moveShapeDown(game.shape);
+  }
+});
 
 function moveShapeDown(shape: Shape) {
-  shape.blocks.forEach(b => b.y++)
+  shape.blocks.forEach(b => b.y++);
 }
 
-var animate = function () {
+function isShapeLandedOnFragment(game: Game): boolean {
+  //number of blocks which is right above any board's fragment
+  return (
+    shape.blocks.filter(
+      b => !!getBoardFragmentByCoords({ board: game.board, x: b.x + 1, y: b.y })
+    ).length > 0
+  );
+}
+
+function isShapeLandedOnBottom(game: Game): boolean {
+  return shape.blocks.some(b => b.y === game.board.height - 1);
+}
+
+function getBoardFragmentByCoords({
+  board,
+  x,
+  y
+}: {
+  board: Board;
+  x: number;
+  y: number;
+}): Block {
+  return board.fragments.find(b => b.x === x && b.y === y);
+}
+var animate = function() {
   requestAnimationFrame(animate);
   Array.from(document.querySelectorAll(".cell"))
     .map(el => ({
       el,
-      canvasBlock: getCanvasBlockByEl(board, el as HTMLElement)
+      canvasBlock: getCanvasBlockByEl(game, el as HTMLElement)
     }))
     .forEach(x => {
       if (x.canvasBlock) {
@@ -60,8 +88,12 @@ var animate = function () {
 
 animate();
 
-function getCanvasBlockByEl(board: Board, el: HTMLElement): Block {
-  return board.blocks.find(
+function getCanvasBlockByEl(game: Game, el: HTMLElement): Block {
+  let boardBlock = game.board.fragments.find(
     b => b.x.toString() === el.dataset.x && b.y.toString() === el.dataset.y
   );
+  let shapeBlock = game.shape.blocks.find(
+    b => b.x.toString() === el.dataset.x && b.y.toString() === el.dataset.y
+  );
+  return boardBlock || shapeBlock;
 }
--- typing/board.ts
+++ typing/board.ts
@@ -2,9 +2,9 @@ import * as t from "io-ts";
 import { BlockIO } from "./block";
 
 export const BoardIO = t.type({
-  blocks: t.array(BlockIO),
+  fragments: t.array(BlockIO),
   width: t.number,
-  height: t.number,
+  height: t.number
 });
 
 export type Board = t.TypeOf<typeof BoardIO>;
