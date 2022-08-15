0cbfbf4af957546150882580d714bddaea25d079
Mikhail Istomin <redeemer@inbox.ru>
2022-08-15 22:20:43 +0300

    clean dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js"
+    filename: "[name].[contenthash].js",
   },
   mode: "development",
   module: {
@@ -23,6 +24,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
