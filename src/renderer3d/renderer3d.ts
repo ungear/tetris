6d592589e01cd984f4e6733a334a1da1d1fff5c8
Mikhail Istomin <mikhail_istomin@epam.com>
2023-08-09 17:47:03 +0400

    get rid of scrollbars

--- src/renderer3d/renderer3d.ts
+++ src/renderer3d/renderer3d.ts
@@ -36,7 +36,8 @@ export class Renderer3d {
     const boardHeightPx = Config.Block.Size * board.height;
 
     this.renderer = new THREE.WebGLRenderer({antialias: true});
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.renderer.setSize(window.innerWidth -1, window.innerHeight-1);
     this.renderer.shadowMap.enabled = true;
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
     document.body.appendChild(this.renderer.domElement);
