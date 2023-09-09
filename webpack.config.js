ab31e22aa327de490376d02cc197b9e8ff5a261b
Mikhail Istomin <mikhail_istomin@epam.com>
2023-09-09 15:30:39 +0400

    enable eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.ts",
@@ -26,5 +27,8 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
+    new ESLintPlugin({
+      extensions: ['ts']
+    })
   ]
 };
